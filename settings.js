f303969d4922b81facfb531f235b29c296362c5c
Sulakkanaq <sulakkanasandeepa@gmail.com>
2025-05-21 12:01:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=5vki1CJZ#syiyTPz-4D2EBllRjW9rYXo-csC5TuV4ymd8Eo2dydU' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=cmVlyYYA#10gS2bAmVWgsWI4KydFfpRPWrb3YAO0sTBdKMF9ysXo' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
